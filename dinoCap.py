55821091667ceca6e54413a0b17cd78876e3df90
Vivek Akella <akellavivek1999@gmail.com>
2019-08-31 16:44:30 +0530

    Screen Captured, Roi Taken, yet to be optimised

--- dinoCap.py
+++ dinoCap.py
@@ -3,17 +3,68 @@ import cv2
 import time
 import pyscreenshot as ImageGrab
 
-def getRoi():
-    pass
+pt1 = (0,0)
+pt2 = (0,0)
+topLeft_clicked = False
+botRight_clicked = False
+flag = 0
+
+def draw_rectangle(event,x,y,flags,param):
+
+    global pt1,pt2,topLeft_clicked,botRight_clicked,flag
+
+    # get mouse click
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        if topLeft_clicked == True and botRight_clicked == True:
+            topLeft_clicked = False
+            botRight_clicked = False
+            flag = 1
+            pt1 = (0,0)
+            pt2 = (0,0)
+
+        if topLeft_clicked == False:
+            pt1 = (x,y)
+            topLeft_clicked = True
+            
+        elif botRight_clicked == False:
+            pt2 = (x,y)
+            botRight_clicked = True
+
+cv2.namedWindow('frame')
+cv2.setMouseCallback('frame', draw_rectangle)
+
+    
 
 def screen_record():
     last_time = time.time()
     while(True):
         # 800x600 windowed mode
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        printscreen =  cv2.cvtColor(np.array(ImageGrab.grab(bbox = (0,80,800,600))),cv2.COLOR_BGR2RGB)
+        if topLeft_clicked:
+            cv2.circle(printscreen, center=pt1, radius=5, color=(0,0,255), thickness=-1)
+        
+        #drawing rectangle
+        if topLeft_clicked and botRight_clicked:
+            cv2.rectangle(printscreen, pt1, pt2, (0, 0, 255), 2)
+        
+        
         print('loop took {} seconds'.format(time.time()-last_time))
         last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        cv2.imshow('frame',printscreen)
+        if(flag == 1 or ((topLeft_clicked == True) and (botRight_clicked == True))):
+            cv2.destroyAllWindows()
+            break
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+    last_time = time.time()
+    while(True):
+        printroi =  np.array(ImageGrab.grab(bbox = (pt1[0],pt1[1],pt2[0],pt2[1])))
+        print('loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
+        cv2.imshow('frame',cv2.cvtColor(printroi, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
